89ec5df9cf6a9fdad93e9b2da323bafbd20f6218
Jack Sawakorn <Sawakorn.ss1@gmail.com>
2025-10-16 18:59:02 +0700

    Formats nuxt.config.ts
    Formats the nuxt.config.ts file to improve readability
    and consistency.

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { defineNuxtConfig } from 'nuxt/config';
+import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   ssr: true,
@@ -28,4 +28,4 @@ export default defineNuxtConfig({
       ]
     }
   }
-});
+})
